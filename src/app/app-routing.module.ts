5caca2d0b57a535c21737ea8fdcee538f7bda6c7
feriani96 <ferianiyasmine3@gmail.com>
2023-10-26 17:54:31 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'articles/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'categories',
+    redirectTo: 'categories/home',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
